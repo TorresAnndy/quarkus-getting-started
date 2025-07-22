930cd0310980b24fdd0e042e33b46d9751e0f24f
TorresAnndy <andygonzales2003@gmail.com>
2025-07-21 19:05:00 -0500

    new file:   docker-composer.yml 	modified:   src/main/java/org/acme/UserResource.java 	new file:   src/main/java/org/acme/micro/User.java 	modified:   src/main/resources/application.properties

--- src/main/java/org/acme/UserResource.java
+++ src/main/java/org/acme/UserResource.java
@@ -1,16 +1,35 @@
 package org.acme;
 
 
+import java.util.List;
+
+import org.acme.micro.User;
+
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.Produces; 
 
 @Path("/users")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class UserResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello(){
         return "Hola desde quarkus get rest";
+    }
+
+    @GET
+    public List<User> getAllUsers(){
+        return User.listAll();
+    }
+
+    @POST
+    @
+    public List<User> postAllUsers(){
+        return User.listAll();
     }
 }
---
+++ src/main/java/org/acme/micro/User.java
@@ -0,0 +1,37 @@
+package org.acme.micro;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+
+
+@Entity
+public class User extends PanacheEntity {
+
+    public String name,email,phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    
+}
